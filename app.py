c763a544611e4681ce3d482038592d530dfd6cb6
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-11 20:20:18 +0300

    feature: flask-templates -template variables

--- app.py
+++ app.py
@@ -9,8 +9,19 @@ def index():
   #### Return a rendered index.html file
   return render_template("index.html")
 
-@app.route("/recipe/<int:id>")
-def recipe(id):
+# THE FOLLOWING CODE IS FOR THE RENDERING TEMPLATES EXERCISE:
+#@app.route("/recipe/<int:id>")
+#def recipe(id):
     
   #### Return a rendered fried_egg.html file
-  return render_template("fried_egg.html")
+#  return render_template("fried_egg.html")
+
+# THE FOLLOWING CODE IS FOR TEMPLATE VARIABLES EXERCISE:
+@app.route("/recipe/<int:id>")
+def recipe(id):
+  #### Add template variables as 
+  #### variable assignment arguments
+  return render_template("recipe.html", 
+                         template_recipe=recipes[id], 
+                         template_description=descriptions[id], 
+                         template_ingredients=ingredients[id])
