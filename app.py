8628047c0e0d70bbd39069107b6887331d67b8c8
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-11 20:47:21 +0300

    feat: flask templates -variable filters

--- app.py
+++ app.py
@@ -17,11 +17,19 @@ def index():
 #  return render_template("fried_egg.html")
 
 # THE FOLLOWING CODE IS FOR TEMPLATE VARIABLES EXERCISE:
-@app.route("/recipe/<int:id>")
-def recipe(id):
+#@app.route("/recipe/<int:id>")
+#def recipe(id):
   #### Add template variables as 
   #### variable assignment arguments
+  #return render_template("recipe.html", 
+                         #template_recipe=recipes[id], 
+                         #template_description=descriptions[id], 
+                         #template_ingredients=ingredients[id])
+
+# THE FOLLOWING CODE IS FOR VARIABLE FILTERS EXERCISE:
+@app.route("/recipe/<int:id>")
+def recipe(id):
   return render_template("recipe.html", 
                          template_recipe=recipes[id], 
-                         template_description=descriptions[id], 
-                         template_ingredients=ingredients[id])
+                         template_ingredients=ingredients[id], 
+                         template_instructions=instructions[id])
